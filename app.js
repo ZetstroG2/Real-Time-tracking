6d1eca03b61ebf73ba31636b32b34e87e3f119a6
Rahul Raj <zetstrog2@gmail.com>
2026-04-20 17:27:29 +0530

    fix: added dynamic port

--- app.js
+++ app.js
@@ -26,6 +26,8 @@ app.get("/", (req, res) => {
     res.render("index");
 })
 
-server.listen(3000, () => {
-    console.log("sever is working")
-})
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
